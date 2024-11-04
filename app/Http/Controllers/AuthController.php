ddb485f6518aa77bd8f6d5e8969bca1309de5d8e
abyanKhairi <khairirisha4@gmail.com>
2024-11-04 08:31:14 +0700

    docs: massage login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -71,7 +71,7 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Unauthorized', 'massage' => 'Invalid credentials'], 401);
         }
 
         return $this->respondWithToken($token);
